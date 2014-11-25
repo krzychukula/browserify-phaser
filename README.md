cae5f767912afc588d4f8cf71f02c1899994900d
Krzysztof Kula <krzychukula@gmail.com>
2014-11-25 08:01:03 +0100

    Add Something visible

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ From: http://invrse.co/build-phaser-with-browserify/
 #Run
 
 `beefy src/game.js:bundle.js --live --open --bug=true`
+
+
+#PixelArt
+
+http://opengameart.org/content/big-space-gun-free-pixel-art-graphics-for-your-game-0
+
+#Issues
+
+* How to access `game` object in states?
--- src/states/boot.js
+++ src/states/boot.js
@@ -1,9 +1,18 @@
+
 module.exports = {
+    preload: function(){
+      game.load.image('mars', '/assets/images/mars.png');
+    },
     create: function(){
-    //This is just like any other Phaser create function
-console.log('Boot was just loaded');
+      //This is just like any other Phaser create function
+      console.log('Boot was just loaded');
+
+
+      this.mars = game.add.sprite(0, 0, 'mars');
     },
     update: function(){
-    //Game logic goes here
+      //Game logic goes here
+      this.mars.x += 1;
+      this.mars.y += 1;
     },
 };
